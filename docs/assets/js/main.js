e67cf92dc4aeff95e26e406a847e158328c62a74
Arthur Lui <luiarthur@gmail.com>
2023-04-10 21:32:00 -0400

    yes

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -3,7 +3,7 @@ import { modes } from "./scales.js"
 function renderModes(id) {
     const node = document.getElementById(id)
     for (const name in modes) {
-        mode = modes[name]
+        const mode = modes[name]
         const li = document.createElement("li")
         li.innerHTML = `${name}: ${mode.scale}`
         node.appendChild(li)
