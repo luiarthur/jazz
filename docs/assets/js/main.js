6e2c7e8e173f94878d77d6116311eaa0f6fb46b3
Arthur Lui <luiarthur@gmail.com>
2023-04-10 23:42:09 -0400

    add basic scales

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,15 +1,114 @@
-import { modes } from "./scales.js"
+// import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js";
+// import "https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js"
+import { modes, scaleOptions, impliedChords } from "./scales.js"
+import "./disqus.js"
+
+function create(tag, html) {
+    const elem = document.createElement(tag)
+    if (html) {
+        elem.innerHTML = html
+    }
+    return elem
+}
 
 function renderModes(id) {
     const node = document.getElementById(id)
+    const thead = create("thead")
+    const tr = create("tr")
+
+    const headers = ["mode", "scale", "from", "start"]
+    headers.forEach(header => {
+        const th = create("th", header)
+        tr.appendChild(th)
+    })
+    thead.appendChild(tr)
+
+    const tbody = create("tbody")
     for (const name in modes) {
         const mode = modes[name]
-        const li = document.createElement("li")
-        li.innerHTML = `${name}: ${mode.scale}`
-        node.appendChild(li)
+        const tr = create("tr")
+
+        headers.forEach(item => {
+            const td = create("td", mode[item])
+            tr.appendChild(td)
+        })
+
+        tbody.appendChild(tr)
     }
+
+    node.appendChild(thead)
+    node.appendChild(tbody)
 }
 
-renderModes("ul-scales")
+function renderScaleOptions(id) {
+    const node = document.getElementById(id)
 
-console.log(modes)
+    // Header
+    const thead = create("thead")
+    const tr = create("tr")
+    const headers = ["Chord Symbol", "Scale"]
+
+    headers.forEach(header => {
+        const th = create("th", header)
+        tr.appendChild(th)
+    })
+    thead.appendChild(tr)
+
+    // Body
+    const tbody = create("tbody")
+    for (const chord in scaleOptions) {
+        const scales = scaleOptions[chord].join(", ")
+        const tr = create("tr")
+
+        tr.appendChild(create("td", chord))
+        tr.appendChild(create("td", scales))
+        tbody.appendChild(tr)
+    }
+
+    node.appendChild(thead)
+    node.appendChild(tbody)
+}
+
+function renderImpliedChords(id) {
+    const node = document.getElementById(id)
+
+    // Header
+    const thead = create("thead")
+    const tr = create("tr")
+
+    const headers = ["Scale", "Chord Symbols"]
+    headers.forEach(header => {
+        const th = create("th", header)
+        tr.appendChild(th)
+    })
+    thead.appendChild(tr)
+
+    // Body
+    const tbody = create("tbody")
+    for (const scale in impliedChords) {
+        const chord = impliedChords[scale].join(", ")
+        const tr = create("tr")
+
+        tr.appendChild(create("td", scale))
+        tr.appendChild(create("td", chord))
+        tbody.appendChild(tr)
+    }
+
+    node.appendChild(thead)
+    node.appendChild(tbody)
+
+ }
+
+function main() {
+    // Main.
+    renderModes("table-scales")
+    renderScaleOptions("table-scale-options")
+    renderImpliedChords("table-implied-chords")
+
+    // Test. Remove when done.
+    console.log(modes)
+    console.log(scaleOptions)
+    console.log(impliedChords)
+}
+
+main()
--- docs/assets/js/scales.js
+++ docs/assets/js/scales.js
@@ -1,5 +1,6 @@
 function makeModes() {
     const modes = {
+        // Major scale modes.
         ionian: {scale: "1 2 3 4 5 6 7", from: "major", start: 1},
         dorian: {scale: "1 2 b3 4 5 6 b7", from: "major", start: 2},
         phrygian: {scale: "1 b2 b3 4 5 b6 b7", from: "major", start: 3},
@@ -7,14 +8,80 @@ function makeModes() {
         mixolydian: {scale: "1 2 3 4 5 6 b7", from: "major", start: 5},
         aeolian: {scale: "1 2 b3 4 5 b6 b7", from: "major", start: 6},
         locrian: {scale: "1 b2 b3 4 b5 b6 b7", from: "major", start: 7},
+        // Minor scale modes.
+        "jazz melodic minor": {
+            scale: "1 2 b3 4 5 6 7", from: "melodic minor asc", start: 1
+        },
+        "dorian b2": {
+            scale: "1 b2 b3 4 5 6 b7", from: "melodic minor asc", start: 2
+        },
+        "lydian augmented": {
+            scale: "1 2 3 #4 #5 6 7", from: "melodic minor asc", start: 3
+        },
+        "lydian dominant": {
+            scale: "1 2 3 #4 5 6 b7", from: "melodic minor asc", start: 4
+        },
+        "mixolydian b6": {
+            scale: "1 2 3 4 5 b6 b7", from: "melodic minor asc", start: 5
+        },
+        "half diminished": {
+            scale: "1 2 b3 4 b5 b6 b7", from: "melodic minor asc", start: 6,
+            alias: ["locrian b2"]
+        },
+        "altered scale": {
+            scale: "1 b2 b3 b4 b5 b6 b7", from: "melodic minor asc", start: 7,
+            alias: ["super locrian"]
+        },
+        // Special Scales
+        "diminished": {
+            scale: "1 b2 b3 b4 b5 5 6 b7", from: "special", start: null,
+            alias: ["half-whole"]
+        }
+        // TODO: Complete the following:
+        // - Major Blues
+        // - Minor Blues
+        // - Major Pentatonic
+        // - Minor Pentatonic
+        // - HW (diminished)
+        // - Major Bebop
+        // - Melodic Minor Bebop
+        // - Harmonic Minor Bebop
+        // - Dominant Bebop: 1 2 3 4 5 b6 b7 7
     }
 
     for (const name in modes) {
-        modes[name].name = name
+        modes[name].mode = name
     }
     return modes
 }
 
 const modes = makeModes()
 
-export { modes }
+const scaleOptions = {
+    "M7": ["ionian", "lydian"],
+    "M6": ["ionian", "lydian"],
+    "m7": ["dorian", "aeolian"],
+    "m6": ["dorian"],
+    "mM7": ["jazz melodic minor"],
+    "7": ["mixolydian", "altered scale", "lydian dominant"],
+    "dim": ["diminished"],
+    "m7b5": ["half diminished", "locrian"],
+    "7alt": ["altered scale"],
+}
+
+function makeImpliedChords() {
+    const impliedChords = {}
+
+    for (const chord in scaleOptions) {
+        const scales = scaleOptions[chord]
+        scales.forEach(scale => {
+            !(scale in impliedChords) && (impliedChords[scale] = [])
+            impliedChords[scale].push(chord)
+        })
+    }
+
+    return impliedChords
+}
+const impliedChords = makeImpliedChords()
+
+export { modes, scaleOptions, impliedChords }
